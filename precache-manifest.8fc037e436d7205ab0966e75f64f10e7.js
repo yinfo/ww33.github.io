2e6c6d1d10252aed3f948192c8d3225aacdd1625
ww33 <yinfo333@gmail.com>
2019-11-22 18:05:25 +0300

    Updates

---
+++ precache-manifest.8fc037e436d7205ab0966e75f64f10e7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "34b93feba6dadd1ff556",
+    "url": "/vue-typescript-admin-template/css/404.04075428.css"
+  },
+  {
+    "revision": "34b93feba6dadd1ff556",
+    "url": "/vue-typescript-admin-template/js/404.adbcac8a.js"
+  },
+  {
+    "revision": "9cb38bb064dcf964144f",
+    "url": "/vue-typescript-admin-template/css/app.547e2723.css"
+  },
+  {
+    "revision": "9cb38bb064dcf964144f",
+    "url": "/vue-typescript-admin-template/js/app.1075d040.js"
+  },
+  {
+    "revision": "5e9093875dd2b71b763b",
+    "url": "/vue-typescript-admin-template/css/chunk-vendors.ee57d822.css"
+  },
+  {
+    "revision": "5e9093875dd2b71b763b",
+    "url": "/vue-typescript-admin-template/js/chunk-vendors.8cb2ba64.js"
+  },
+  {
+    "revision": "0fe47198817290ad3c5e",
+    "url": "/vue-typescript-admin-template/css/dashboard.a6cd872a.css"
+  },
+  {
+    "revision": "0fe47198817290ad3c5e",
+    "url": "/vue-typescript-admin-template/js/dashboard.b3610db6.js"
+  },
+  {
+    "revision": "b69ff09cb1a1b8196e9b",
+    "url": "/vue-typescript-admin-template/css/form.02b8675b.css"
+  },
+  {
+    "revision": "b69ff09cb1a1b8196e9b",
+    "url": "/vue-typescript-admin-template/js/form.edb212d9.js"
+  },
+  {
+    "revision": "d3e682e6d081dfca945c",
+    "url": "/vue-typescript-admin-template/css/login.4770ef2a.css"
+  },
+  {
+    "revision": "d3e682e6d081dfca945c",
+    "url": "/vue-typescript-admin-template/js/login.78b8d81c.js"
+  },
+  {
+    "revision": "841a1af1201190032975",
+    "url": "/vue-typescript-admin-template/js/menu1.4ae79270.js"
+  },
+  {
+    "revision": "631d5bc48a043bc71e36",
+    "url": "/vue-typescript-admin-template/js/menu1-1.1d81b313.js"
+  },
+  {
+    "revision": "ea6e96e76770c2937ca5",
+    "url": "/vue-typescript-admin-template/js/menu1-2.5829848e.js"
+  },
+  {
+    "revision": "5d03005deea54782b409",
+    "url": "/vue-typescript-admin-template/js/menu1-2-1.799d906f.js"
+  },
+  {
+    "revision": "961bfb8a724dc9f19576",
+    "url": "/vue-typescript-admin-template/js/menu1-2-2.c2258359.js"
+  },
+  {
+    "revision": "ea7103a7517b778eb84c",
+    "url": "/vue-typescript-admin-template/js/menu1-3.f4254763.js"
+  },
+  {
+    "revision": "a6c493e10bc3cc3bbbb1",
+    "url": "/vue-typescript-admin-template/js/menu2.99bd2713.js"
+  },
+  {
+    "revision": "13384f37d6f74a2e29d2",
+    "url": "/vue-typescript-admin-template/js/table.6a126719.js"
+  },
+  {
+    "revision": "c1fa484ddd4773c81ce7",
+    "url": "/vue-typescript-admin-template/js/tree.e6416216.js"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/vue-typescript-admin-template/img/404-cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-typescript-admin-template/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-typescript-admin-template/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/vue-typescript-admin-template/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "ac8c68dcf06f322fe4c399ec73e6ad8b",
+    "url": "/vue-typescript-admin-template/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-typescript-admin-template/robots.txt"
+  }
+];
